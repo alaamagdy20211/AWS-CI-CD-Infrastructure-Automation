cf16deb69a1c8c05691d865ca0effd4cbf96f431
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-22 04:47:10 +0600

    update in my app

--- ec2.2.tf
+++ ec2.2.tf
@@ -3,4 +3,7 @@ ami = var.ami
 instance_type = var.instance_type
   subnet_id = module.network.subnets["priv_subnet_1"].id
   vpc_security_group_ids = [aws_security_group.allow_traffic_limited.id]
+  provisioner "local-exec"{
+  command = "echo ${self.private_ip} > ec2_private_ip.txt"
+}
 }
