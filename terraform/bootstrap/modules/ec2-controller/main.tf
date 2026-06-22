675de4a4c82ae12515820d797b1ec7b780fb1ca0
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-22 08:25:56 +0600

    create controller  module for jenkins

---
+++ terraform/bootstrap/modules/ec2-controller/main.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] 
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "controller" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "jenkins-controller"
+    Role = "jenkins-controller"
+  }
+}
