269b1131d3562cb85eb0097d11921ff71573c89a
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-22 08:32:17 +0600

    create agent  module for jenkins

---
+++ terraform/bootstrap/modules/ec2-agent/variables.tf
@@ -0,0 +1,15 @@
+variable "subnet_id" {
+  type        = string
+}
+
+variable "security_group_id" {
+  type        = string
+}
+
+variable "key_name" {
+  type        = string
+}
+
+variable "instance_type" {
+  type        = string
+}
