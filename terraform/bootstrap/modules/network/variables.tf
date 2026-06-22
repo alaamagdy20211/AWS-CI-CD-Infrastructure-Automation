e5e29bf46f17c4659b52a43aa4f9fd09106a234f
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-22 08:20:53 +0600

    create network module for jenkins

--- terraform/bootstrap/modules/network/variables.tf
+++ terraform/bootstrap/modules/network/variables.tf
@@ -1,14 +1,11 @@
 variable "vpc_cidr" {
-  description = "CIDR block for the bootstrap VPC"
   type        = string
 }
 
 variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
   type        = string
 }
 
 variable "availability_zone" {
-  description = "AZ to place the subnet in"
   type        = string
 }
