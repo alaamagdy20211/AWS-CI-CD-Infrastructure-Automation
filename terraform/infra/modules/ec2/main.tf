e0260c52bae758632914ffebdaf2dd3bf2d181c4
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-24 09:29:15 +0600

    add pass for ec2 instances

--- terraform/infra/modules/ec2/main.tf
+++ terraform/infra/modules/ec2/main.tf
@@ -3,7 +3,8 @@ ami = var.ami
 instance_type = var.instance_type
 subnet_id = var.subnets["public_subnet_1"].id
 vpc_security_group_ids = [var.bastion_sg_id]
-associate_public_ip_address = true      
+associate_public_ip_address = true  
+key_name =var.key_name    
 provisioner "local-exec"{
   command = "echo ${self.public_ip} > ip_bastion.txt"
 }
@@ -13,9 +14,9 @@ provisioner "local-exec"{
 resource "aws_instance" "app" {
 ami = var.ami
 instance_type = var.instance_type
-  # subnet_id = module.network.subnets["priv_subnet_1"].id
   subnet_id = var.subnets["priv_subnet_1"].id
   vpc_security_group_ids = [var.app_sg_id]
+  key_name =var.key_name    
   provisioner "local-exec"{
   command = "echo ${self.private_ip} > ec2_private_ip.txt"
 }
