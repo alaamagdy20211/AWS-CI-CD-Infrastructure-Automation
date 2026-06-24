dfc4c28eecc291089c9fe2fb64f9b72d02477176
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-24 08:06:27 +0600

    update one the name of subnet group

--- terraform/infra/modules/rds/subnet.tf
+++ terraform/infra/modules/rds/subnet.tf
@@ -1,5 +1,5 @@
-resource "aws_db_subnet_group" "wp_rds_subnet_group" {
-  name       = "wp_rds"
+resource "aws_db_subnet_group" "wp_rds_subnet_groupp" {
+  name       = "wp_rdss"
   subnet_ids = var.subnet_ids
   tags = {
     Name = "WP RDS"
