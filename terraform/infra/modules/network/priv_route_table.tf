9162e233f9f213228e8a5634f6d54571394e9b83
Alaa magdy <alaamagdy3008@gmail.com>
2026-06-25 09:32:42 +0600

    Enable nodejs 18

--- terraform/infra/modules/network/priv_route_table.tf
+++ terraform/infra/modules/network/priv_route_table.tf
@@ -5,3 +5,9 @@ resource "aws_route_table" "private_rt" {
     Name = "private_route_table"
   }
 }
+
+resource "aws_route" "private_default" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
