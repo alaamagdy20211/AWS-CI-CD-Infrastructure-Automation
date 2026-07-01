d52785c35fe5f54e6b36e013ea138d68484c1bc4
Alaa magdy <alaamagdy3008@gmail.com>
2026-07-01 15:39:52 +0600

    change target port in alb

--- terraform/infra/modules/alb/main.tf
+++ terraform/infra/modules/alb/main.tf
@@ -8,7 +8,7 @@ locals {
 resource "aws_security_group" "alb_sg" {
   name        = "alb-sg"
   description = "Allow incoming HTTP connections to the ALB"
-  vpc_id      = var.vpc_id   
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 80
@@ -23,13 +23,17 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "alb-sg"
+  }
 }
 
 resource "aws_lb" "application_lb" {
   name               = "app-alb"
   internal           = false
-  ip_address_type    = "ipv4"
   load_balancer_type = "application"
+  ip_address_type    = "ipv4"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = local.public_subnet_ids
 
@@ -40,18 +44,29 @@ resource "aws_lb" "application_lb" {
 
 resource "aws_lb_target_group" "target_group" {
   name        = "app-tg"
-  port        = 80
+  port        = 3000
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = var.vpc_id
 
   health_check {
-    interval            = 10
-    path                = "/"
+    enabled             = true
     protocol            = "HTTP"
+    path                = "/db"
+    port                = "traffic-port"
+    interval            = 10
     timeout             = 5
     healthy_threshold   = 5
     unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "app-tg"
   }
 }
 
@@ -61,21 +76,21 @@ resource "aws_lb_listener" "alb_listener" {
   protocol          = "HTTP"
 
   default_action {
+    type             = "forward"
     target_group_arn = aws_lb_target_group.target_group.arn
-    type              = "forward"
   }
 }
 
 resource "aws_lb_target_group_attachment" "ec2_attach" {
   target_group_arn = aws_lb_target_group.target_group.arn
   target_id        = var.app_id
-  port             = 80
+  port             = 3000
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_from_alb" {
-  security_group_id           = var.app_sg_id
+  security_group_id            = var.app_sg_id
   referenced_security_group_id = aws_security_group.alb_sg.id
   from_port                    = 3000
   to_port                      = 3000
-  ip_protocol                  =  "tcp"
+  ip_protocol                  = "tcp"
 }
--- terraform/infra/modules/security/security_rule2.tf
+++ terraform/infra/modules/security/security_rule2.tf
@@ -26,10 +26,3 @@ resource "aws_vpc_security_group_egress_rule" "app_allow_all_outbound" {
   ip_protocol       = "-1"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_anywhereee" {
-  security_group_id = aws_security_group.allow_traffic_limited.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
